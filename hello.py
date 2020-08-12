daa1271b7848c3ad6de8bfa9c2a51d4f17151497
Shao-Ching Huang <sch@ucla.edu>
2020-08-11 21:01:24 -0700

    rename func1 to func_1 (branch1)

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-def func1():
+def func_1():
     print('func1')
 
 def func2():
@@ -8,7 +8,7 @@ def func3():
     print('func3')
 
 def main():
-    func1()
+    func_1()
     func2()
     func3()
 
