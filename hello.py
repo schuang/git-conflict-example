bf680b716b5bc263dbdb3942c22bc9308e19aa28
Shao-Ching Huang <sch@ucla.edu>
2020-08-11 20:59:24 -0700

    first commit (master)

---
+++ hello.py
@@ -0,0 +1,16 @@
+def func1():
+    print('func1')
+
+def func2():
+    print('func2')
+
+def func3():
+    print('func3')
+
+def main():
+    func1()
+    func2()
+    func3()
+
+if __name__ == '__main__':
+    main()
